fa8c39db16335fb17584c97bd5a30afd2ca272d1
pashpashpash <pashints@usc.edu>
2018-04-15 19:13:58 -0700

    made some image changes

--- src/cucumber/CucumberRunner.java
+++ src/cucumber/CucumberRunner.java
@@ -1,15 +1,19 @@
 package cucumber;
- 
+
 import org.junit.runner.RunWith;
 import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
- 
+
 @RunWith(Cucumber.class)
 @CucumberOptions(
 		features = "Feature"
 		,glue={"stepDefinition"}
 		)
- 
+
 public class CucumberRunner {
- 
+
 }
+
+//	 This code will copy part of one picture to another picture
+//	public static Picture makeCollage (Picture pict, Picture pict2, Picture pict3)
+//	{

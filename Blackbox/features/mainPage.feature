ee72b273ff2c218c76511ff16847338bffe0e5e0
johndwyer <johndwyer127>
2018-04-19 21:13:43 -0700

    mainpage blackbox

---
+++ Blackbox/features/mainPage.feature
@@ -0,0 +1,34 @@
+Feature: Main Page
+
+Background:
+
+	Given I am on the main page
+
+Scenario: Topic Box
+
+	Then I should see a text field named "topic"
+
+Scenario: Shape Box
+
+	Then I should see a text field named "shape"
+
+Scenario: Collage Width
+
+	Then I should see a text field named "width"
+
+Scenario: Collage Height
+
+	Then I should see a text field named "height"
+
+Scenario: Borders Box
+
+	Then I should see a checkbox named "bordersBox"
+
+Scenario: Rotation Box
+
+	Then I should see a checkbox named "rotationsBox"
+
+Scenario: Save Box
+
+	Then I should see a checkbox named "saveBox"
+	
--- Blackbox/features/step_definitions/mainPage.rb
+++ Blackbox/features/step_definitions/mainPage.rb
@@ -2,22 +2,28 @@ Given(/^I am on the page "([^"]*)"$/) do |arg1|
   visit arg1
 end
 
-Then(/^I should see a text field "([^"]*)"$/) do |arg1|
-  page.find_by_id('topic').should == arg1
+Then(/^I should see a text field named "topic"/) do
+  expect(page).to have_field("topic")
 end
 
-Then(/^I should see a text field "([^"]*)"$/) do |arg1|
-  page.find_by_id('shape').should == arg1
+Then(/^I should see a text field named "shape"$/) do
+  expect(page).to have_field("shape")
 end
 
-Then(/^I should see a select field named "([^"]*)"$/) do |arg1|
-  page.find_by_id('options').should == arg1
+Then(/^I should see a text field named "width"$/) do
+  expect(page).to have_field("width")
 end
 
-Then(/^I should see a button named "([^"]*)"$/) do |arg1|
-  page.find_by_id('save').should == arg1
+Then(/^I should see a text field named "height"$/) do
+  expect(page).to have_field("height")
 end
 
-Then(/^I should see a button named "([^"]*)"$/) do |arg1|
-  page.find_by_id('build').should == arg1
+Then(/^I should see a checkbox named "bordersBox"$/) do
+  expect(page).to have_field("bordersBox")
 end
+
+Then(/^I should see a checkbox named "rotationsBox"$/) do
+  expect(page).to have_field("rotationsBox")
+
+Then(/^I should see a checkbox named "saveBox"$/) do
+    expect(page).to have_field("saveBox")

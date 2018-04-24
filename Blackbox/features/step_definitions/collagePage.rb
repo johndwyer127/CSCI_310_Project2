90d8a3343cb54909e6b7345131d53454ce1782da
johndwyer <johndwyer127>
2018-04-24 16:01:09 -0700

    collage pageblackbox

---
+++ Blackbox/features/step_definitions/collagePage.rb
@@ -0,0 +1,15 @@
+Given(/^I am on the collage display page$/) do
+  visit #put servlet address here
+end
+
+Then(/^I should see an export as png button$/) do
+  expect(page).to have_selector("button", :id =>"exportButtonPNG")
+end
+
+Then(/^I should see an export as pdf button$/) do
+  expect(page).to have_selector("button", :id =>"exportButtonPDF")
+end
+
+Then(/^I should have a secure connection$/) do
+  expect(page).to have_current_path("https", url: true)
+end

3ccdd8e372ae33ae8f39e3cac537f5f20f7248a3
pashpashpash <pashints@usc.edu>
2018-04-26 20:02:27 -0700

    GOT EM WORKING

--- Blackbox/features/support/env.rb
+++ Blackbox/features/support/env.rb
@@ -1,10 +1,12 @@
-require 'capybara'
-require 'capybara/cucumber'
-require 'rspec'
+require('capybara')
+require('capybara/cucumber')
+require('r_spec')
+
+
 
 
 Capybara.register_driver :chrome do |app|
-  Capybara::Selenium: :Driver.new(app, :browser => :chrome, :driver_path=>"/usr/lib/chromium-browser/chromedriver")
+  Capybara::Selenium::Driver.new(app, :browser => :chrome)
 end
 
 Capybara.default_driver = :chrome
